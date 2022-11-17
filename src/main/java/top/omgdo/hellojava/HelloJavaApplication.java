acbff42649c9b3a84b4126d06d24003517d8111e
omgdo <atbisr@163.com>
2022-11-17 16:13:43 +0800

    测试1

---
+++ src/main/java/top/omgdo/hellojava/HelloJavaApplication.java
@@ -0,0 +1,13 @@
+package top.omgdo.hellojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/omgdo/hellojava/HelloJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package top.omgdo.hellojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
